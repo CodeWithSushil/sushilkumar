d3a6dedfd6e0edb898d6d9480aa9cef15bffccc8
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-09-14 21:41:27 +0530

    add meta tages

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,19 +1,65 @@
 <?= $this->extend('layouts/main') ?>
 
 <?= $this->section('head') ?>
+<!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap">
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="robots" content="index, follow" />
+  <meta name="theme-color" content="#FF2D20">
+  <meta http-equiv="X-Content-Type-Options" content="nosniff">
+  <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
+ 
+<meta name="description" content="Sushil Kumar | Full Stack PHP/Laravel Developer, Freelancer, open source contributor, content creator on YouTube.">
+<meta name="author" content="Sushil Kumar">
 
-<meta name="description"
-      content="<?= esc($description ?? '') ?>">
+<!-- Open Graph / Facebook -->
+  <meta property="og:type" content="profile">
+  <meta property="og:url" content="https://sushilkumar.onrender.com/">
+  <meta property="og:title" content="Sushil Kumar | Portfolio">
+  <meta property="og:description" content="Sushil Kumar aka Code With Sushil is a PHP Developer.">
+  <meta property="og:image" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta property="og:image:secure_url" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+  <meta property="og:image:alt" content="Sushil Kumar, PHP Developer">
+  <meta property="og:site_name" content="Sushil Kumar">
+  <meta property="og:locale" content="en_US">
+  <meta property="profile:first_name" content="Sushil">
+  <meta property="profile:last_name" content="Kumar">
+  
+  <!-- Twitter / X -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@CodeSushil">
+  <meta name="twitter:creator" content="@CodeSushil">
+  <meta name="twitter:url" content="https://sushilkumar.onrender.com/">
+  <meta name="twitter:title" content="Sushil Kumar | Portfolio">
+  <meta name="twitter:description" content="Sushil Kumar aka Code With Sushil is a PHP Developer.">
+  <meta name="twitter:image" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta name="twitter:image:src" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta name="twitter:image:alt" content="Sushil Kumar, PHP Developer">
+
+<link rel="icon" type="image/png"  href="favicon.ico" />
+<link rel="apple-touch-icon" type="image/png" href="favicon.ico" >
+<link rel="canonical" href="https://sushilkumar.onrender.com/">
+
+<!-- Bootstrap 5.3.8 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Icons -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
 
 <?= $this->endSection() ?>
 
-
 <?= $this->section('content') ?>
 
-<h1><?= esc($heading ?? 'Welcome') ?></h1>
+<h1>Welcome</h1>
 
 <p>
-    <?= esc($message ?? '') ?>
+this is a page
 </p>
 
 <?= $this->endSection() ?>
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,10 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title><?= esc($title ?? 'My Website') ?></title>
-
+    <title><?= esc($title ?? 'Sushil Kumar | Full Stack PHP Developer') ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="index, follow" />
+    <meta name="theme-color" content="#FF2D20">
+    <meta http-equiv="X-Content-Type-Options" content="nosniff">
+    <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
     <?= $this->renderSection('head') ?>
+    <meta name="author" content="Sushil Kumar">
+
+<!-- Open Graph / Facebook -->
+  <meta property="og:type" content="profile">
+  <meta property="og:url" content="https://sushilkumar.onrender.com/">
+  <meta property="og:title" content="Sushil Kumar | Portfolio">
+  <meta property="og:description" content="Sushil Kumar aka Code With Sushil is a PHP Developer.">
+  <meta property="og:image" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta property="og:image:secure_url" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1200">
+  <meta property="og:image:height" content="630">
+  <meta property="og:image:alt" content="Sushil Kumar, PHP Developer">
+  <meta property="og:site_name" content="Sushil Kumar">
+  <meta property="og:locale" content="en_US">
+  <meta property="profile:first_name" content="Sushil">
+  <meta property="profile:last_name" content="Kumar">
+
+  <!-- Twitter / X -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@CodeSushil">
+  <meta name="twitter:creator" content="@CodeSushil">
+  <meta name="twitter:url" content="https://sushilkumar.onrender.com/">
+  <meta name="twitter:title" content="Sushil Kumar | Portfolio">
+  <meta name="twitter:description" content="Sushil Kumar aka Code With Sushil is a PHP Developer.">
+  <meta name="twitter:image" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta name="twitter:image:src" content="https://sushilkumar.onrender.com/og/master.png">
+  <meta name="twitter:image:alt" content="Sushil Kumar, PHP Developer">
+
+<link rel="icon" type="image/png"  href="favicon.ico" />
+<link rel="apple-touch-icon" type="image/png" href="favicon.ico" >
+<link rel="canonical" href="https://sushilkumar.onrender.com/">
+
+<!-- Bootstrap 5.3.8 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Icons -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -20,6 +62,5 @@
     <?= $this->include('include/footer') ?>
 
     <?= $this->renderSection('scripts') ?>
-
 </body>
 </html>
